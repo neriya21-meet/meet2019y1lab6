3f856d8f3bb9070135005457920c0e94c4dc997e
neriya21-meet <neriya21@meet.mit.edu>
2019-07-28 11:32:09 -0400

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,15 @@
+##total = 0
+##for number in range(1, 10 + 1):
+##    print(number)
+##    total = total + number
+##print(total)
+##
+
+def add_numbers():
+    total= 0
+    for number in range(1, 10 +10):
+        print(number)
+        total = total + number
+    return (total)
+answer= add_numbers()
+print(answer)
