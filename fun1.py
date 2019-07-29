5863c4e3653ffbec88a0cf1caef77170056307a2
neriya21-meet <neriya21@meet.mit.edu>
2019-07-29 03:30:15 -0400

    Stopping here

--- fun1.py
+++ fun1.py
@@ -5,11 +5,20 @@
 ##print(total)
 ##
 
-def add_numbers():
-    total= 0
-    for number in range(1, 10 +10):
-        print(number)
-        total = total + number
-    return (total)
-answer= add_numbers()
-print(answer)
+##def add_numbers():
+##    total= 0
+##    for number in range(1, 10 +10):
+##        print(number)
+##        total = total + number
+##    return (total)
+##answer= add_numbers()
+##print(answer)
+
+def add_num2(start, end):
+	total=0	
+	for number in range (start,end+1):
+		print(number)
+		total+= number
+	return (total)
+test1 = add_num2(333,777)
+print(test1)
---
+++ fun2.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.goto(0,0)
+
+def up():
+    print("jjhjh")
+    
+turtle.onkey(up, 'up')
+turtle.goto(0,0)
+turtle.listen()
+
+turtle.mainloop
